e066a2768b295d97b90436a44beff50f47cebf2e
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-09-25 17:01:43 +0300

    init Domain.Core

---
+++ src/BuildingBlocks/Domain.Core/Primitives/AggregateRoot.cs
@@ -0,0 +1,27 @@
+﻿using CSharpFunctionalExtensions;
+using Domain.Core.Events;
+
+namespace Domain.Core.Primitives;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : IComparable<TId>
+{
+    protected AggregateRoot(TId id)
+        : base(id)
+    {
+    }
+
+    private readonly List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
+
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent newEvent)
+    {
+        _domainEvents.Add(newEvent);
+    }
+
+    public void ClearEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
---
+++ src/BuildingBlocks/Domain.Core/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Core.Primitives;
+using Domain.Core.UnitOfWork;
+
+namespace Domain.Core.Repositories;
+
+public interface IRepository<TAggregateRoot, TId>
+    where TAggregateRoot : AggregateRoot<TId>
+    where TId : IComparable<TId>
+{
+    IUnitOfWork UnitOfWork { get; }
+}
